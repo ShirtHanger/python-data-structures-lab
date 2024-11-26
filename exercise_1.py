87d8772343db26365d4b48858dd9fcc815d98f1a
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-11-25 20:32:56 -0600

    boilterplate

---
+++ exercise_1.py
@@ -0,0 +1,11 @@
+# Exercise 1: List and Indexing
+#
+# Create a list named students containing at least three student names (strings).
+# Assign the second student’s name to a variable named first_student.
+# Assign the last student’s name to a variable named last_student.
+
+def manage_students():
+    # your code here
+
+# Call the function and print the result
+print('Exercise 1:', manage_students())
---
+++ exercise_3.py
@@ -0,0 +1,9 @@
+# Exercise 3: Slicing Tuples
+#
+# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods():
+    # your code here
+
+# Call the function and print the result
+print('Exercise 3:', slice_foods())
